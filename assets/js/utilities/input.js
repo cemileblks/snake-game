89aca8de6038c7bf7b1d9c5c1ebf70d784616dab
Cemileb <cemilebalkas@gmail.com>
2023-12-29 12:57:14 +0000

    added better graphics for small screens and move function when snake is small

--- assets/js/utilities/input.js
+++ assets/js/utilities/input.js
@@ -1,3 +1,4 @@
+import { snakeBody } from "./utils.js";
 const keysContainer = document.getElementById('keys-container');
 let inputDirection = { x: 0, y: 0 };
 let lastInputDirection = { x: 0, y: 0 };
@@ -18,15 +19,31 @@ function handleButtonClick(event) {
 function handleKeyDown(event) {
     switch (event.key) {
         case "ArrowUp":
+            if(snakeBody.length < 2){
+                handleInputSmallSnake("up-btn");
+                break;
+            }
             handleInput("up-btn");
             break;
         case "ArrowDown":
+            if(snakeBody.length < 2){
+                handleInputSmallSnake("down-btn");
+                break;
+            }
             handleInput("down-btn");
             break;
         case "ArrowLeft":
+            if(snakeBody.length < 2){
+                handleInputSmallSnake("left-btn");
+                break;
+            }
             handleInput("left-btn");
             break;
         case "ArrowRight":
+            if(snakeBody.length < 2){
+                handleInputSmallSnake("right-btn");
+                break;
+            }
             handleInput("right-btn");
             break;
         default:
@@ -63,6 +80,24 @@ function handleInput(buttonId) {
             break;
         case "right-btn":
             if (lastInputDirection.x !== 0) break;
+            inputDirection = { x: 1, y: 0 };
+            break;
+        default:
+            break;
+    }
+}
+function handleInputSmallSnake(buttonId) {
+    switch (buttonId) {
+        case "up-btn":
+            inputDirection = { x: 0, y: -1 };
+            break;
+        case "down-btn":
+            inputDirection = { x: 0, y: 1 };
+            break;
+        case "left-btn":
+            inputDirection = { x: -1, y: 0 };
+            break;
+        case "right-btn":
             inputDirection = { x: 1, y: 0 };
             break;
         default:
