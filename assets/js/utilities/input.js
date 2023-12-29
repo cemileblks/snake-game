77bf535d16f60d3b3e1647e7738eee293a194a37
Cemileb <cemilebalkas@gmail.com>
2023-12-29 13:27:26 +0000

    added instructions for game

--- assets/js/utilities/input.js
+++ assets/js/utilities/input.js
@@ -13,34 +13,37 @@ window.addEventListener('keydown', handleKeyDown);
 
 function handleButtonClick(event) {
     const buttonId = event.target.id;
+    if (snakeBody.length < 2) {
+        handleInputSmallSnake(buttonId);
+    }
     handleInput(buttonId);
 }
 
 function handleKeyDown(event) {
     switch (event.key) {
         case "ArrowUp":
-            if(snakeBody.length < 2){
+            if (snakeBody.length < 2) {
                 handleInputSmallSnake("up-btn");
                 break;
             }
             handleInput("up-btn");
             break;
         case "ArrowDown":
-            if(snakeBody.length < 2){
+            if (snakeBody.length < 2) {
                 handleInputSmallSnake("down-btn");
                 break;
             }
             handleInput("down-btn");
             break;
         case "ArrowLeft":
-            if(snakeBody.length < 2){
+            if (snakeBody.length < 2) {
                 handleInputSmallSnake("left-btn");
                 break;
             }
             handleInput("left-btn");
             break;
         case "ArrowRight":
-            if(snakeBody.length < 2){
+            if (snakeBody.length < 2) {
                 handleInputSmallSnake("right-btn");
                 break;
             }
